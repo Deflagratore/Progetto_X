62f3bc643c487d17c7738ece2f8462369f784d41
Deflagratore <fnicodemo2007@gmail.com>
2025-03-07 12:56:40 +0100

    piccoli aggiustamenti

--- visualizzatore_dadi.py
+++ visualizzatore_dadi.py
@@ -4,13 +4,13 @@ from dadi import Dado
 dado = Dado()
 dado2 = Dado(10)
 
-risultati = []
+risultati: list[int] = []
 
 for rollata in range(1_000_000): #calcola i risultati del lancio dei dadi
     risultato = dado.roll() + dado2.roll()
     risultati.append(risultato)
 
-frequenze = []
+frequenze: list[int] = []
 
 risultato_massimo = dado.num_sides + dado2.num_sides
 
@@ -20,16 +20,20 @@ for value in poss_result: #calcola la frequenza dei risultati
     frequenza = risultati.count(value)
     frequenze.append(frequenza)
 
-titolo = "Risultati di 1 milione di lanci da 2 dadi (1=D6, 2=D10)" #aggiunge il titolo al grafico
-labels = {"x": "Risultato", "y": "Frequenza del risultato"} #questo codice aggiunge all'asse x e y dei titoli
+titolo: str = "Risultati di 1 milione di lanci da 2 dadi (1=D6, 2=D10)" #aggiunge il titolo al grafico
+labels: dict[str,str] = {"x": "Risultato", "y": "Frequenza del risultato"} #questo codice aggiunge all'asse x e y dei titoli
 
 fig = px.bar(x=poss_result, y=frequenze, title=titolo, labels=labels) #implementa i cambiamenti fatti sopra
 
 fig.update_layout(xaxis_dtick=1) #sistema il layout per far si che si vedano tutti i numeri dell'asse x
 
-domanda = input("Vuoi salvare il grafico? ")
+domanda = input("Vuoi salvare il grafico? ").lower()
 
 if domanda == "si":
-    fig.write_html("grafico_lancio_dati.html") #salva la figura
+    fig.write_html("grafico_lancio_dati.html") #salva la figura come file html
+    print("Figura salvata con successo come file html")
+elif domanda == "sni":    
+    fig.write_image(file="grafico_lancio_dadi", format="png") #non so perche ma ci impiega ore
+    print("Figura salvata con successo come png")
 else:
     fig.show() #mostra la figura
